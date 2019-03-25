31512ad5377466989924c09c8dd971d16e51ade8
Vlad <vovaturcan4@gmail.com>
2019-03-25 23:34:21 +0200

    able to make queries to my prisma db right from the node server

--- src/prisma.js
+++ src/prisma.js
@@ -2,5 +2,13 @@ import { Prisma } from "prisma-binding";
 
 const prisma = new Prisma({
   typeDefs: "src/generated/prisma.graphql",
-  endpoint: "localhost:4466"
+  endpoint: "http://localhost:4466"
+});
+
+prisma.query.users(null, "{ id name posts { title id } }").then(data => {
+  console.log(JSON.stringify(data, undefined, 4));
+});
+
+prisma.query.comments(null, "{ id text author { name id } }").then(data => {
+  console.log(JSON.stringify(data, undefined, 2));
 });
