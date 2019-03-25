05ad5ad8ae22189de1177fa6e1790957346b33ca
Vlad <vovaturcan4@gmail.com>
2019-03-26 00:49:02 +0200

    refactored to use async await and performing prisma changes with .exists and .mutation

--- src/prisma.js
+++ src/prisma.js
@@ -5,10 +5,60 @@ const prisma = new Prisma({
   endpoint: "http://localhost:4466"
 });
 
-prisma.query.users(null, "{ id name posts { title id } }").then(data => {
-  console.log(JSON.stringify(data, undefined, 4));
-});
+// prisma.query .mutation .subscription .exists
 
-prisma.query.comments(null, "{ id text author { name id } }").then(data => {
-  console.log(JSON.stringify(data, undefined, 2));
-});
+const createPostForUser = async (authorId, data) => {
+  const userExists = await prisma.exists.User({
+    id: authorId
+  });
+
+  if (!userExists) {
+    throw Error("Author not found");
+  }
+
+  const post = await prisma.mutation.createPost(
+    {
+      data: {
+        ...data,
+        author: {
+          connect: {
+            id: authorId
+          }
+        }
+      }
+    },
+    "{ author { id name email posts { title id published } } }"
+  );
+
+  return post;
+};
+
+const updatePostForUser = async (postId, data) => {
+  const postExists = await prisma.exists.Post({
+    id: postId
+  });
+
+  if (!postExists) {
+    throw Error("Post not found");
+  }
+
+  const post = await prisma.mutation.updatePost(
+    { data, where: { id: postId } },
+    "{author { id name email posts { title id published } }}"
+  );
+
+  return post;
+};
+
+// updatePostForUser("cjtovsp2r00je0790cula5pjp", {
+//   published: false,
+//   title: "somehting new"
+// })
+//   .then(data => console.log(JSON.stringify(data, undefined, 2)))
+//   .catch(e => console.log(e));
+
+// createPostForUser("cjtn0selb00ol0890dc699btx", {
+//   title: "Great books to read",
+//   body: "The art of war",
+//   published: true
+// });
